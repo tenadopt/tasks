87676259ccd524c82f2c4df89ab91ca283ed7a45
Tenadopt <yury.ignatiev@gmail.com>
2023-05-30 17:44:57 +0300

    add NewComponentFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React, {useState} from 'react';
 import './App.css';
+import {NewComponentFilter} from "./components/NewComponentFilter";
+
+export type FilterType = 'All'|'Dollars'|'RUBLS'
+
 
 function App() {
 
-   let [a, setA] = useState(1);
+   const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ]);
 
-   const onClickHandler = () => {
-       setA(++a);
-       console.log(a)
+   const [filterValue, setFilter] = useState<FilterType>('All')
+
+   const onClickFilterHandler = (filterValue: FilterType) => {
+       setFilter(filterValue)
    }
 
-   const onClickHandler1 = () => {
-       setA(0);
-       console.log(a)
-   }
+    let currentMoney = money;
+
+    if (filterValue === 'Dollars') {
+        currentMoney = money.filter(el=>el.banknots === filterValue)
+    }
+
+    if (filterValue === 'RUBLS') {
+        currentMoney = money.filter(el=>el.banknots === filterValue)
+    }
 
     return (
-        <div>
-            <h1>{a}</h1>
-            <button onClick={onClickHandler}>+1</button>
-            <button onClick={onClickHandler1}>0</button>
-        </div>
+        <>
+            <NewComponentFilter currentMoney={currentMoney} onClickFilterHandler={onClickFilterHandler}/>
+        </>
     );
 }
 
